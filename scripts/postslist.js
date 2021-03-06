8d87a67acc3ec596cb118725ae0eddda6839863a
Abhijeet Verma <vickyverma4u@gmail.com>
2021-03-06 10:59:40 +0530

    implement Part B visuals

---
+++ scripts/postslist.js
@@ -0,0 +1,9 @@
+function openDeletePostModal() {
+  var modal = document.getElementById("delete-post-modal");
+  modal.style.display = "block";
+}
+
+function closeDeletePostModal() {
+  var modal = document.getElementById("delete-post-modal");
+  modal.style.display = "none";
+}
