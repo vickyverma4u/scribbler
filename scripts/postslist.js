6622d175857e398f92d682b7c3e1c074d8442bad
Abhijeet Verma <vickyverma4u@gmail.com>
2021-03-07 17:08:06 +0530

    add delete post functionality as per Part B

--- scripts/postslist.js
+++ scripts/postslist.js
@@ -1,9 +1,18 @@
-function openDeletePostModal() {
+let targetElement;
+
+function openDeletePostModal(e) {
+  targetElement = e.parentElement.parentElement.parentElement.parentElement;
   var modal = document.getElementById("delete-post-modal");
   modal.style.display = "block";
 }
 
 function closeDeletePostModal() {
+  targetElement = null;
   var modal = document.getElementById("delete-post-modal");
   modal.style.display = "none";
+}
+
+function deletePost() {
+  document.getElementById("delete-post-modal").style.display = "none";
+  targetElement.remove();
 }
